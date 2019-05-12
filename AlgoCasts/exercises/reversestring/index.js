bfb3c7302bb8cb0530373aa748c0cc6ac7d207ea
BudacaB <budaca.b@gmail.com>
2019-05-13 01:31:10 +0300

    reverse string 3rd solution

--- AlgoCasts/exercises/reversestring/index.js
+++ AlgoCasts/exercises/reversestring/index.js
@@ -10,12 +10,18 @@
 //const reverse = (str) => { return str.split('').reverse().join(''); }
 
 //Solution #2
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+//Solution #3
 function reverse(str) {
-    let reversed = '';
-    for (let character of str) {
-        reversed = character + reversed;
-    }
-    return reversed;
+    return str.split('').reduce((rev, char) => char + rev, '');
 }
+
 
 module.exports = reverse;
